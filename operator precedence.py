312b4c77bb53e6f4cd7be68f4e56d16d96583470
gloriousainembabazi <gloriousainembabazi66@gmail.com>
2024-10-07 15:13:51 +0300

    Add files via upload

---
+++ operator precedence.py
@@ -0,0 +1,27 @@
+#OPERATOR PRECEDENCE ,control flow structures, conditional statement ,loops 
+#define this describes the order in which operation are performed in an expreession, 
+#operators with high pre cidence are always excuted first
+
+#ans
+
+result= 3*4+5
+print(result)
+
+result1= 3*4+5-1
+print(result1)
+
+result2= 3*(4+5)-1
+print(result2)
+
+result3= 3*(4+5)/1
+print(result3)
+
+result4= 5*3**2
+print(result4)
+
+result5= (5+3)*2**2-10/2
+print(result5)
+
+
+
+
